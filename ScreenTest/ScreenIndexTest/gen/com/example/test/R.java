4c77478ee8145fb0933b28cd055dff95e3bb0156
Mike McDevitt <turnpikemike@gmail.com>
2011-11-12 18:34:53 -0500

    added rotation index labels.

--- ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
+++ ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
@@ -14,10 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int RotationLayout=0x7f050004;
         public static final int linearLayout1=0x7f050001;
+        public static final int pitchIndex=0x7f050005;
+        public static final int rollIndex=0x7f050006;
         public static final int testID=0x7f050000;
         public static final int xIndex=0x7f050002;
         public static final int yIndex=0x7f050003;
+        public static final int yawIndex=0x7f050007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
