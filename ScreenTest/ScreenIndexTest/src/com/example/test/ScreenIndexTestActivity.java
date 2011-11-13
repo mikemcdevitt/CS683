b1c0d024b27c02863d15491ae11e307b931a65f2
Mike McDevitt <turnpikemike@gmail.com>
2011-11-12 19:56:39 -0500

    audiosynth task still wasn't working.

--- ScreenTest/ScreenIndexTest/src/com/example/test/ScreenIndexTestActivity.java
+++ ScreenTest/ScreenIndexTest/src/com/example/test/ScreenIndexTestActivity.java
@@ -1,10 +1,6 @@
 package com.example.test;
 
 import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.TextView;
@@ -37,42 +33,7 @@ public class ScreenIndexTestActivity extends Activity {
         
         return true;
     }
+
     
-    private class AudioSynthesisTask extends AsyncTask<Void, Void, Void> {
-	    @Override
-	    protected Void doInBackground(Void... params) {
-	      final int SAMPLE_RATE = 22000;
-
-	      int minSize = AudioTrack.getMinBufferSize(SAMPLE_RATE,
-	          AudioFormat.CHANNEL_CONFIGURATION_MONO,
-	          AudioFormat.ENCODING_PCM_16BIT);
-
-	      AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-	          SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-	          AudioFormat.ENCODING_PCM_16BIT, minSize,
-	          AudioTrack.MODE_STREAM);
-
-	      audioTrack.play();
-
-	      short[] buffer = new short[minSize];
-	      
-	      
-	      
-	      float angular_frequency = (float) R.string.frequency
-	          / SAMPLE_RATE;
-	      float angle = 0;
-
-	   //   while (keepGoing) {
-	        for (int i = 0; i < buffer.length; i++) {
-	          buffer[i] = (short) (Short.MAX_VALUE * ((float) Math
-	              .sin(angle)));
-	          angle += angular_frequency;
-	        }
-	        audioTrack.write(buffer, 0, buffer.length);
-	   //   }
-
-	      return null;
-	    }
-	  }
     
 }
