16355a81e29de1aa98fcd69ebb58dc6e1fe7d3ad
Mike McDevitt <turnpikemike@gmail.com>
2011-11-13 23:43:58 -0500

    adding some string array resources for some of the frequency/note/pitch mappings we'll be using.

--- ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
+++ ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
@@ -8,20 +8,24 @@
 package com.example.test;
 
 public final class R {
+    public static final class array {
+        public static final int base_frequencies=0x7f050001;
+        public static final int pitches=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int RotationLayout=0x7f050004;
-        public static final int linearLayout1=0x7f050001;
-        public static final int pitchIndex=0x7f050005;
-        public static final int rollIndex=0x7f050006;
-        public static final int testID=0x7f050000;
-        public static final int xIndex=0x7f050002;
-        public static final int yIndex=0x7f050003;
-        public static final int yawIndex=0x7f050007;
+        public static final int RotationLayout=0x7f060004;
+        public static final int linearLayout1=0x7f060001;
+        public static final int pitchIndex=0x7f060005;
+        public static final int rollIndex=0x7f060006;
+        public static final int testID=0x7f060000;
+        public static final int xIndex=0x7f060002;
+        public static final int yIndex=0x7f060003;
+        public static final int yawIndex=0x7f060007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
