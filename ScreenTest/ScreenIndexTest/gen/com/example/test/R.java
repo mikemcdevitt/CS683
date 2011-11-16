7593a14806d81b4635483fc18b1a7c69f65bcf62
Mike McDevitt <turnpikemike@gmail.com>
2011-11-16 17:29:37 -0500

    adding in the code from Zeke's computer from 11/15/11.

--- ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
+++ ScreenTest/ScreenIndexTest/gen/com/example/test/R.java
@@ -9,8 +9,10 @@ package com.example.test;
 
 public final class R {
     public static final class array {
-        public static final int base_frequencies=0x7f050001;
-        public static final int pitches=0x7f050000;
+        public static final int base_frequencies=0x7f050003;
+        public static final int pitches=0x7f050002;
+        public static final int start_list_options=0x7f050000;
+        public static final int start_list_options_values=0x7f050001;
     }
     public static final class attr {
     }
@@ -18,32 +20,49 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int RotationLayout=0x7f060004;
-        public static final int linearLayout1=0x7f060001;
-        public static final int pitchIndex=0x7f060005;
-        public static final int rollIndex=0x7f060006;
-        public static final int testID=0x7f060000;
-        public static final int xIndex=0x7f060002;
-        public static final int yIndex=0x7f060003;
-        public static final int yawIndex=0x7f060007;
+        public static final int RotationLayout=0x7f080004;
+        public static final int linearLayout1=0x7f080001;
+        public static final int menu_prefs=0x7f08000a;
+        public static final int pitchIndex=0x7f080005;
+        public static final int rollIndex=0x7f080007;
+        public static final int rollIndexLabel=0x7f080006;
+        public static final int testID=0x7f080000;
+        public static final int text1=0x7f080009;
+        public static final int xIndex=0x7f080002;
+        public static final int yIndex=0x7f080003;
+        public static final int yawIndex=0x7f080008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int mainmenu=0x7f070000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int frequency=0x7f04000d;
-        public static final int pitchIndex=0x7f040008;
-        public static final int pitchIndexLabel=0x7f040007;
-        public static final int rollIndex=0x7f04000a;
-        public static final int rollIndexLabel=0x7f040009;
-        public static final int rotationIndices=0x7f040006;
-        public static final int screenIndices=0x7f040001;
-        public static final int xIndex=0x7f040003;
-        public static final int xIndexLabel=0x7f040002;
-        public static final int yIndex=0x7f040005;
-        public static final int yIndexLabel=0x7f040004;
-        public static final int yawIndex=0x7f04000c;
-        public static final int yawIndexLabel=0x7f04000b;
+        public static final int app_name=0x7f060000;
+        public static final int default_start_value_preference=0x7f060003;
+        public static final int dialogTitle=0x7f060007;
+        public static final int frequency=0x7f060015;
+        public static final int listSummary=0x7f060005;
+        public static final int listTitle=0x7f060004;
+        public static final int menu_prefs_title=0x7f060008;
+        public static final int pitchIndex=0x7f060010;
+        public static final int pitchIndexLabel=0x7f06000f;
+        public static final int prefSummary=0x7f060002;
+        public static final int prefTitle=0x7f060001;
+        public static final int rollIndex=0x7f060012;
+        public static final int rollIndexLabel=0x7f060011;
+        public static final int rotationIndices=0x7f06000e;
+        public static final int screenIndices=0x7f060009;
+        public static final int start_list=0x7f060006;
+        public static final int xIndex=0x7f06000b;
+        public static final int xIndexLabel=0x7f06000a;
+        public static final int yIndex=0x7f06000d;
+        public static final int yIndexLabel=0x7f06000c;
+        public static final int yawIndex=0x7f060014;
+        public static final int yawIndexLabel=0x7f060013;
+    }
+    public static final class xml {
+        public static final int screenoptions=0x7f040000;
     }
 }
--- ScreenTest/ScreenIndexTest/src/com/example/test/ScreenIndexTestActivity.java
+++ ScreenTest/ScreenIndexTest/src/com/example/test/ScreenIndexTestActivity.java
@@ -1,21 +1,88 @@
 package com.example.test;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 public class ScreenIndexTestActivity extends Activity {
-    /** Called when the activity is first created. */
+    private TextView tv = null;
+    private CircleView cv = null;
+    private Resources resources;
+    float x = 0;
+    float y = 0;
+	private static final float width = 30;
+	private static final float height = 30;
+	private int baseFreqIndex;
+	double freq;
+	AudioSynthesisTask audioSynth;
+	boolean keepGoing = false;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
         
+        
+        //setContentView(R.layout.main);
+        this.cv = new CircleView(this, x,y);
+        this.setContentView(this.cv);
+        
+        tv = (TextView)findViewById(R.id.pitchIndex);
+        
+        resources = this.getResources();
+        setOptionText();    
     }
     
-    float x = 0;
-    float y = 0;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.mainmenu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) { 
+    	if (item.getItemId() == R.id.menu_prefs) {
+    		Intent intent = new Intent();
+    		intent.setClass(this, com.example.test.ScreenPreferencesActivity.class);
+    		startActivityForResult(intent, 0);
+    	}
+    	return true;
+    }
+    
+    @Override
+    public void onActivityResult(int reqCode, int resCode, Intent data) { 
+    	super.onActivityResult(reqCode, resCode, data);
+    	setOptionText();
+    }
+    
+    private void setOptionText() {
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    	String option = prefs.getString(resources.getString(R.string.start_list), resources.getString(R.string.default_start_value_preference));
+    	
+    	String[] optionText = resources.getStringArray(R.array.start_list_options);
+    	baseFreqIndex = Integer.valueOf(option);
+    	//tv.setText("preference for start is " + option + " (" + optionText[Integer.parseInt(option)] + ")" );
+    }
+    
     
     @Override 
     public boolean onTouchEvent(MotionEvent e) {
@@ -25,15 +92,106 @@ public class ScreenIndexTestActivity extends Activity {
 
     	x = e.getX();
         y = e.getY();
+        
+
+        
+        double f_start = 27.5 * Math.pow(2,baseFreqIndex);
+        double f_final = f_start * 2;
+        
+        double x_fraction = x/1280.0;
+        
+        freq = f_start + x_fraction*(f_final - f_start);
        
-        TextView tvX = (TextView)findViewById(R.id.xIndex); 
-        TextView tvY = (TextView)findViewById(R.id.yIndex); 
-        tvX.setText("" + x);
-        tvY.setText("" + y);
+        //TextView tvX = (TextView)findViewById(R.id.xIndex); 
+        //TextView tvY = (TextView)findViewById(R.id.yIndex); 
+        //TextView tvFLabel = (TextView)findViewById(R.id.rollIndexLabel);
+        //TextView tvFreq = (TextView)findViewById(R.id.rollIndex);
+        //tvX.setText("" + x);
+        //tvY.setText("" + y);
+        //tvFreq.setText("" + freq);
+        //tvFLabel.setText("Output Frequency:");
+        
+        
+         if (e.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+            keepGoing = true;
+            audioSynth = new AudioSynthesisTask();
+            audioSynth.execute();
+          } else if (e.getAction() == android.view.MotionEvent.ACTION_MOVE) {
+        	  
+        	  // NEED A BETTER WAY TO HANDLE MOUSE MOVE EVENTS.
+        	  
+        	  //audioSynth.cancel(true);
+        	  //audioSynth = new AudioSynthesisTask();
+        	  //keepGoing = true;
+        	  //audioSynth.execute();
+          } else if (e.getAction() == android.view.MotionEvent.ACTION_UP) {
+        	  keepGoing = false;
+          }
+        
+        
+        this.setContentView(new CircleView(this, x, y));
         
         return true;
     }
+    
+    
+    
+    private static class CircleView extends View {
+    	private  ShapeDrawable circle = new ShapeDrawable();
+    	private int left, top, right, bottom;
+    	private static final float width = 30;
+    	private static final float height = 30;
+    	
+    	public CircleView(Context context, float x, float y) {
+    		super(context);
+    		setFocusable(true);
+    		this.left = Math.round(x - width/2);
+    		this.top = Math.round(y - height/2);
+    		this.right = Math.round(x + width/2);
+    		this.bottom = Math.round(y + height/2);
+    		this.circle = new ShapeDrawable(new OvalShape());
+    		this.circle.getPaint().setColor(Color.CYAN);
+    	}
+    	
+    	@Override
+    	protected void onDraw(Canvas canvas) {
+    		circle.setBounds(left, top, right, bottom);
+    		this.circle.draw(canvas);
+    	}
+    }
+    
+    private class AudioSynthesisTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+          final int SAMPLE_RATE = 11025;
 
-    
-    
+          int minSize = AudioTrack.getMinBufferSize(SAMPLE_RATE,
+              AudioFormat.CHANNEL_CONFIGURATION_MONO,
+              AudioFormat.ENCODING_PCM_16BIT);
+
+          AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+              SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+              AudioFormat.ENCODING_PCM_16BIT, minSize,
+              AudioTrack.MODE_STREAM);
+
+          audioTrack.play();
+
+          short[] buffer = new short[minSize];
+
+          float angular_frequency = (float) (2 * Math.PI) * (float)freq
+              / SAMPLE_RATE;
+          float angle = 0;
+
+          while (keepGoing) {
+            for (int i = 0; i < buffer.length; i++) {           	
+              buffer[i] = (short) (Short.MAX_VALUE * ((float) Math
+                  .sin(angle)));
+              angle += angular_frequency;
+            }
+            audioTrack.write(buffer, 0, buffer.length);
+          }
+
+          return null;
+        }
+      }
 }
